baaf651d5386759b70754c4362b17df8021f7a77
pratham40 <122166099+pratham40@users.noreply.github.com>
2025-05-16 20:54:54 +0530

    add nodemon for development and implement basic server setup

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+
+
+const app = express();
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
