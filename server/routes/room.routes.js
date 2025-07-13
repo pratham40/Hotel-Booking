b5cda86594c10fb8a91c964f465b28f60cdbb2e8
pratham40 <122166099+pratham40@users.noreply.github.com>
2025-07-13 10:16:58 +0530

    correct import statement for room controller in room routes

--- server/routes/room.routes.js
+++ server/routes/room.routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import upload from '../middlewares/multer.middleware.js';
 import {authMiddleware} from "../middlewares/auth.middleware.js"
-import { createRoom, getOwnerRooms, getRooms, toggleRoomAvailability } from '../controllers/room.controller';
+import { createRoom, getOwnerRooms, getRooms, toggleRoomAvailability } from '../controllers/room.controller.js';
 const roomRouter = express.Router();
 
 roomRouter.post("/",upload.array("images",4),authMiddleware,createRoom)
